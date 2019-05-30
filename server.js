cb95579954a80385074eb6ba17bd5605f82779ba
deLeonRodrigo <rodri230998@gmail.com>
2019-05-30 16:52:01 -0500

    fixed local enviorment

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-const port = 80;
+const port = 2000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.listen(port, () => {
-    console.log('We are live on ' + port);
-});
 
 app.post('/code', (req, res) => {
     console.log(req.body.code);
     res.send("okay")
+});
+
+
+
+app.listen(port, () => {
+    console.log('We are live on ' + port);
 });
