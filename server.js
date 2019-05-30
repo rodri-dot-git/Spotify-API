435a1478054ef88dbb1365e8dc5c39c351e213a7
deLeonRodrigo <rodri230998@gmail.com>
2019-05-30 16:57:20 -0500

    db added

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-const port = 2000;
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://rodridlc:Mauricio10@cluster0-gitot.azure.mongodb.net/spotify?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+    const collection = client.db("spotify").collection("users");
+    // perform actions on the collection object
+    client.close();
+});
+
+const port = 80;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -10,8 +19,6 @@ app.post('/code', (req, res) => {
     console.log(req.body.code);
     res.send("okay")
 });
-
-
 
 app.listen(port, () => {
     console.log('We are live on ' + port);
