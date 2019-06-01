feb04d1174264204c9659277839de46026c57ad0
deLeonRodrigo <rodri230998@gmail.com>
2019-06-01 17:31:33 -0500

    commited deploy

--- server.js
+++ server.js
@@ -2,18 +2,22 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://rodridlc:Mauricio10@cluster0-gitot.azure.mongodb.net/spotify?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-    const collection = client.db("spotify").collection("users");
-    // perform actions on the collection object
-    client.close();
-});
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://rodridlc:Mauricio10@cluster0-gitot.azure.mongodb.net/spotify?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, {
+//     useNewUrlParser: true
+// });
+// client.connect(err => {
+//     const collection = client.db("spotify").collection("users");
+//     // perform actions on the collection object
+//     client.close();
+// });
 app.use(express.static('views'))
 const port = 8080;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.post('/code', (req, res) => {
     console.log(req.body.code);
@@ -26,4 +30,16 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log('We are live on ' + port);
+});
+
+var my_client_id = '010fde68a6df41048c87cc0855a2f5ce';
+var redirect_uri = 
+
+app.get('/login', function (req, res) {
+    var scopes = 'user-read-private user-read-email';
+    res.redirect('https://accounts.spotify.com/authorize' +
+        '?response_type=code' +
+        '&client_id=' + my_client_id +
+        (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+        '&redirect_uri=' + encodeURIComponent(redirect_uri));
 });
